8074d4b5c8622b2a0c077d423275c4ca64eda46e
Manshi Sharma <Manshi.Sharma@tavisca.com>
2022-06-24 17:16:59 +0530

    assignment-1

---
+++ BookApi/Contracts/BookContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace BookApi.Contracts
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Book> Book { get; set; }
+    }
+}
